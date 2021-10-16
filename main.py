d15300a0c32459625491f9ec2ae42ac0393e6b2d
Padsala Trushal <57517785+Trushal2004@users.noreply.github.com>
2021-10-16 10:14:35 +0530

    Add a function for create screenshot folder and folder with date name

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import pyautogui
 import os
-
+import datetime
+import time
 
 Desktoppath = os.path.join((os.environ['USERPROFILE']),'Desktop')
-print(Desktoppath)
-
-# print(os.getcwd())
+# print(Desktoppath) 
 sspath = os.path.join(Desktoppath,'Screenshot')
-print(sspath)
-os.mkdir(sspath)
+# print(sspath)
+
+def ssfolder():
+
+    if os.path.isdir(sspath) == True:
+        print("ssfolder  exists")
+    else:
+        os.mkdir(sspath)
+        print("ssfolder created")
+
+ssfolder()
+
+
+
+def datefolder():
+    
+    datefoldername = time.strftime("%d-%m-%Y")
+    datefolderpath =  os.path.join(sspath,datefoldername)
+    # print(datefolderpath)
+
+
+    if os.path.isdir(datefolderpath) == True:
+        print("datefolder exists")
+    
+    else:
+        os.mkdir(datefolderpath)
+        print("datefolder created")
+
+datefolder()
