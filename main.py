6df740b07f1c54c0e96ccedc130767bf7b6ccd7a
Padsala Trushal <57517785+Trushal2004@users.noreply.github.com>
2021-10-16 11:24:14 +0530

    Add function for screenshot
    Add Screenshot function for take screenshot and store in screenshot folder with datewise folder and save as image named time also add some comments for understanding code

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 import pyautogui
 import os
-import datetime
 import time
 
+# For getting desktop path
 Desktoppath = os.path.join((os.environ['USERPROFILE']),'Desktop')
 # print(Desktoppath) 
+
+# For get Screenshot folder path
 sspath = os.path.join(Desktoppath,'Screenshot')
 # print(sspath)
 
+# For make folder named Screenshot in desktop
 def ssfolder():
-
     if os.path.isdir(sspath) == True:
-        print("ssfolder  exists")
+        # print("ssfolder  exists")
+        pass
     else:
         os.mkdir(sspath)
-        print("ssfolder created")
-
-ssfolder()
+        # print("ssfolder created")
 
 
+# for get datewise folder path
+datefoldername = time.strftime("%d-%m-%Y")
+datefolderpath =  os.path.join(sspath,datefoldername)
+# print(datefolderpath)
 
+
+# for make datewise folder in screenshot folder
 def datefolder():
-    
-    datefoldername = time.strftime("%d-%m-%Y")
-    datefolderpath =  os.path.join(sspath,datefoldername)
-    # print(datefolderpath)
-
-
     if os.path.isdir(datefolderpath) == True:
-        print("datefolder exists")
-    
+        # print("datefolder exists")
+        pass
     else:
         os.mkdir(datefolderpath)
-        print("datefolder created")
+        # print("datefolder created")
 
+
+# for each image name with time
+imagename = time.strftime(r"%H-%M-%S.png")
+# print(imagename)
+
+# for each image path
+imagepath = os.path.join(datefolderpath,imagename)
+# print(imagepath)
+
+# for create screenshot in store in spacif location
+def makescreenshot():
+    pyautogui.screenshot(imagepath)
+
+
+
+
+ssfolder()
 datefolder()
+makescreenshot()
